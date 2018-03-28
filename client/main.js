84a855caa59025f80a30ca91d1875f0954256747
Nathan <nathanmolle25@gmail.com>
2018-03-28 10:32:13 +0200

    Push branch pour beta register

--- client/main.js
+++ client/main.js
@@ -1,5 +1,8 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Accounts } from 'meteor/accounts-base';
+
+
 
 if (Meteor.isClient) {
     Template.registerPage.events({
@@ -7,13 +10,36 @@ if (Meteor.isClient) {
             event.preventDefault();
             var emailVar = event.target.registerEmail.value;
             var passwordVar = event.target.registerPassword.value;
+            var surnameVar = event.target.registerSurname.value;
+            var nameVar = event.target.registerName.value;
+
             console.log("Form register submitted.");
+
             Accounts.createUser({
                 email: emailVar,
-                password: passwordVar
+                password: passwordVar,
+                surname: surnameVar,
+                name: nameVar
+                
+            }, function(error){
+                if (error){
+                    console.log(error);
+                }else {
+                    Meteor.call('sendVerificationLink',(error,response) => {
+                        if (error){
+                            console.log(error);
+                        }else {
+                            console.log("mail send");
+                        }
+                    })
+                }
             });
-        }
-    });
+    }});
+
+
+
+
+
 
     Template.loginPage.events({
       'submit form': function(event) {
@@ -21,11 +47,13 @@ if (Meteor.isClient) {
           var emailVar = event.target.loginEmail.value;
           var passwordVar = event.target.loginPassword.value;
           Meteor.loginWithPassword(emailVar, passwordVar);
+          // Redirection
           console.log("Form login submitted.");
       }
     });
 
     Template.header.events({
+
       'click .logout': function(event){
           event.preventDefault();
           Meteor.logout();
--- server/main.js
+++ server/main.js
@@ -3,6 +3,9 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
   
+  /* Imports for server-side startup go here. */
+
+process.env.MAIL_URL = "smtp://postmaster@sandbox7182c35800f14d0ea13ed1dad692b926.mailgun.org:ad1f50961b55a2f2014730938bd11466-833f99c3-5c74770e@smtp.mailgun.org:2525";
 
 });
 
@@ -10,9 +13,20 @@ Meteor.publish('films', function(title) {
   return Films.find({flagged: false, title: title});
 });
 
+Meteor.methods({
+  sendVerificationLink() {
+    console.log("test serveur side");
+    let userId = Meteor.userId();
+    console.log(userId);
+    if ( userId ) {
+      return Accounts.sendVerificationEmail( userId );
+    }
+  }
+});
 
 Meteor.publish('allFilmsByTitle', function(){
   return Films.find({'title':'Meteor'}, {fields: {
     date: false
   }});
 });
+// test
