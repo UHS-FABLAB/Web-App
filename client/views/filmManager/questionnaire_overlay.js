ed07817bfcc13f519bcdbfa7361d4471d8ff7945
DESKTOP-3G06E02\Antoine <akeribin@gmail.com>
2018-04-26 04:28:22 +0200

    merge fin

--- client/views/filmManager/questionnaire_overlay.js
+++ client/views/filmManager/questionnaire_overlay.js
@@ -6,7 +6,7 @@
       */
      Template.questionnaireOverlay.onCreated(function bodyOnCreated() {
 
-    
+
       Meteor.subscribe('votes');
       Meteor.subscribe("responses");
 
@@ -57,8 +57,8 @@
         if(formLength > 2){
          $("#new_question_form").children().last().remove();
         }
-        
-  
+
+
       },
 
       // Lorsqu'on valide le formulaire
@@ -69,6 +69,7 @@
         var mon_media = Session.get('media');
         var mediaIdReponse = $('#select_media_output_1').val();
         var indexReponse = "";
+        var idMedia = Session.get('mediaId');
         event.preventDefault();
 
         //Stocke les données du questionnaire sous forme d'Array
@@ -86,48 +87,58 @@
         var reponse = {
           content: "",
           defaultChoice: false,
-          mediaId: mon_media._id,
+          mediaId: "",
           voteId: ""
         }
 
         // Check si un vote existe déjà pour ce film, si oui l'update sinon l'insert dans la BDD
         if(!isVote){
           //insert le vote
-          Meteor.call('voteInsert', vote, mon_media);
+          Meteor.call('voteInsert', vote, mon_media, function(error, voteId) {
+            Medias.update(idMedia, {$set: {voteId: voteId }});
+            // Pour chaque réponse, si la réponse n'est pas vide créer une réponse dans la bdd
+            console.log(formData)
+            $.each(formData, function(idx, item) {
+              console.log(item)
+              if(item.name!="question"){
+                if(item.name == "answer_1"){
+                  reponse.defaultChoice=true;
+                }else{
+                  reponse.defaultChoice=false;
+                  indexReponse = item.name.split('_')[1];
+                  mediaIdReponse = $('#select_media_output_'+indexReponse).val();
+                  reponse.mediaId=mediaIdReponse;
+                }
+                reponse.voteId = voteId
+                reponse.mediaId=mediaIdReponse;
+                reponse.content= item.value;
+                Meteor.call('responseInsert', reponse,function (error, responseId) {
 
-        // Pour chaque réponse, si la réponse n'est pas vide créer une réponse dans la bdd
-        formData.forEach(function(item) {
-          if(item.name!="question"){
-            if(item.name == "answer_1"){
-              reponse.defaultChoice=true;
-            }else{
-              reponse.defaultChoice=false;
-              indexReponse = $(item.name).split('_')[1];
-              mediaIdReponse = $('#select_media_output_'+indexReponse).vel();
-              reponse.mediaId=mediaIdReponse;
-            }
-            reponse.mediaId=mediaIdReponse;
-            reponse.content= item.value;
-            Meteor.call('responseInsert', reponse);
+                  console.log(error, responseId)
+                });
 
-          }
-        });
+              }
+            });
+          });
+
 
         }else {
           // update le vote
-          Meteor.call('voteUpdate',vote, mon_media)   
+          Meteor.call('voteUpdate',vote, mon_media, function(error, voteId){
+            //update les réponses
+            var arrayResponses = Session.get('responses');
+            formData.forEach(function(item) {
+              if(item.name!="question"){
 
-          //update les réponses
-          var arrayResponses = Session.get('responses');
-          formData.forEach(function(item) {
-            if(item.name!="question"){
-              
-            }
-          });
+              }
+            });
+          })
+
+
 
         }
 
-        
+
 
 
 
