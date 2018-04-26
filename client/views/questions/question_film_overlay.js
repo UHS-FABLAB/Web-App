ac63aea2ffdabbebf62c63da2c8b0ae3a54e213e
ThomasGuigard <thomasguigard@gmail.com>
2018-04-26 09:52:20 +0200

    PUSSSSSHHHHH

--- client/views/questions/question_film_overlay.js
+++ client/views/questions/question_film_overlay.js
@@ -1,11 +1,12 @@
 
 Template.questionFilmOverlay.helpers({
   responses: function() {
+    console.log(Responses.findOne({voteId: this.idQ}))
     return Responses.find({voteId: this.idQ});
   },
   question: function(){
     console.log(this.idQ);
-    console.log(Questions.findOne({_id: this.idQ}))
-    return Questions.findOne({_id: this.idQ});
+    console.log(Votes.findOne({_id: this.idQ}))
+    return Votes.findOne({_id: this.idQ});
   }
 })
